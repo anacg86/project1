fe2014a140a075ba6d767f1d61a4eee38c6118bb
Callanthas <lord_of_light1@hotmail.com>
2018-11-30 20:19:34 -0600

    final version Youtube API, need to fix CSS

--- assets/scripts/youtube.js
+++ assets/scripts/youtube.js
@@ -1,96 +1,71 @@
-//Initiate Youtube Video API 
+var config = {
+  apiKey: "AIzaSyBTUty_2eQIF51UpzBShEe0XvRUwsdtEzc",
+  authDomain: "weightloss-c69df.firebaseapp.com",
+  databaseURL: "https://weightloss-c69df.firebaseio.com",
+  projectId: "weightloss-c69df",
+  storageBucket: "weightloss-c69df.appspot.com",
+  messagingSenderId: "360811521364"
+              };
+firebase.initializeApp(config);
+var database = firebase.database();
 
-$(document).ready(function(){
+database.ref().on("child_added", function(childSnapshot) {
+  $(document).ready(function(){
+      $.ajax({
+      url: "https://www.googleapis.com/youtube/v3/search",
+      method: "GET",
+      data: {
+        part: 'snippet',
+        maxResults: 7,
+        q: childSnapshot.val().selGender + " " + childSnapshot.val().selValue + " exercise",
+        type: 'video',
+        key: 'AIzaSyC_54O2ZQp7CNs5wDhwMKlqG5oOSmj7MK4'
+         }
+      })
+      .then((response) => {
+  console.log(response);
   
-    var key = 'AIzaSyAxnhYiYDjPXFbKgal4GDQDukGzqJV81PI';    
-    var playlistId = childSnapshot.val().selValue;
-    var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+  //show first video
+$('#video').append(`
+                      <iframe width="600" height="350" src="https://www.youtube.com/embed/${response.items[0].id.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                  `);
+ //show the next 6 videos
+    $.each(response.items, function (i, item) {
+      if(i === 0)
+      return;
   
-      var options = {
-          part: 'snippet',
-          key: key,
-          maxResults: 5,
-          playlistId: playlistId
-      }
-  
-      loadVids(options);
-  
-      function loadVids(videoOptions) {
-          $.getJSON(URL, videoOptions, function (data) {
-              console.log(options);
-              var id = data.items[0].snippet.resourceId.videoId;
-              mainVid(id);
-              resultsLoop(data);
-          });
-      }
-  
-      function mainVid(id) {
+    var thumb = item.snippet.thumbnails.medium.url;
+    var title = item.snippet.title;
+    var desc = item.snippet.description.substring(0, 100);
+    var vid = item.snippet.id;
+
+    
+    $('main').append(`
+                    <article class="item" data-key="${item.id.videoId}">
+
+                        <img src="${thumb}" alt="" class="thumb">
+                        <div class="details">
+                            <h4>${title}</h4>
+                            <p>${desc}</p>
+                        </div>
+
+                    </article>
+                `);
+});
+
+$('main').on('click', 'article', function () {
+  var id = $(this).attr('data-key');
+  mainVid(id);
+  function mainVid(id) {
           $('#video').html(`
                       <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                   `);
       }
-  
-          
-      function resultsLoop(data) {
-          $('main').empty();
-          $.each(data.items, function (i, item) {
-  
-              var thumb = item.snippet.thumbnails.medium.url;
-              var title = item.snippet.title;
-              var desc = item.snippet.description.substring(0, 100);
-              var vid = item.snippet.resourceId.videoId;
-  
-              
-              $('main').prepend(`
-                              <article class="item" data-key="${vid}">
-  
-                                  <img src="${thumb}" alt="" class="thumb">
-                                  <div class="details">
-                                      <h4>${title}</h4>
-                                      <p>${desc}</p>
-                                  </div>
-  
-                              </article>
-                          `);
-          });
-      }
-  
-          // CLICK EVENT
-      $('main').on('click', 'article', function () {
-          var id = $(this).attr('data-key');
-          mainVid(id);
-      });
-  
-      $('.womanWorkout').on('click', function () {
-          console.log("hello")
-          var options = {
-              part: 'snippet',
-              key: key,
-              maxResults: 5,
-              playlistId: 'PLChOO_ZAB22WuyDODJ3kjJiU0oQzWOTyb'
-          }
-          loadVids(options);
-      });
-      
-      $('.OutdoorWorkout').on('click', function () {
-          console.log("hello")
-          var options = {
-              part: 'snippet',
-              key: key,
-              maxResults: 5,
-              playlistId: 'PLA_I2ay5YcUUr8_rE-DlVknObkJ6-Rn3q'
-          }
-          loadVids(options);
-      });
-      
-      $('.gymWorkout').on('click', function () {
-          console.log("hello")
-          var options = {
-              part: 'snippet',
-              key: key,
-              maxResults: 5,
-              playlistId: 'PLDULdzOzi3Sprr_e2Op69eoPNUaq5Z7Ev'
-          }
-          loadVids(options);
-      });
-  });
+});
+
+
+});
+
+
+});
+})
